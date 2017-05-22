805c781d2a7507b9120bd7e48429debb1b530b9d
Vladimir <vladimir.stanciu92@gmail.com>
2017-05-22 21:00:10 +0300

    Access Granter v1.3 Filter active permissions so there are no duplicates.

--- src/main/java/com/vladimirstanciu/accessgranter/service/PermissionServiceImpl.java
+++ src/main/java/com/vladimirstanciu/accessgranter/service/PermissionServiceImpl.java
@@ -5,15 +5,18 @@ import com.vladimirstanciu.accessgranter.domain.Role;
 import com.vladimirstanciu.accessgranter.domain.RolePermission;
 import com.vladimirstanciu.accessgranter.domain.UserRole;
 import com.vladimirstanciu.accessgranter.repository.PermissionRepository;
-import com.vladimirstanciu.accessgranter.repository.RoleRepository;
 import com.vladimirstanciu.accessgranter.repository.UserRepository;
-import com.vladimirstanciu.accessgranter.repository.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+
+import static java.util.Comparator.comparingLong;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
 
 /**
  * Created by Vladimir on 5/21/2017.
@@ -23,12 +26,6 @@ public class PermissionServiceImpl implements PermissionService {
 
     @Autowired
     private PermissionRepository permissionRepository;
-
-    @Autowired
-    private UserRoleRepository userRoleRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -41,11 +38,11 @@ public class PermissionServiceImpl implements PermissionService {
         return permissionRepository.findAll();
     }
 
-    public List<Permission> getActivePermissions(Long id){
+    public List<Permission> getActivePermissions(Long id) {
         return permissionRepository.findActivePermissions(id);
     }
 
-    public List<Permission> getAvailablePermissions(Long id){
+    public List<Permission> getAvailablePermissions(Long id) {
         return permissionRepository.findAvailablePermissions(id);
     }
 
@@ -53,14 +50,16 @@ public class PermissionServiceImpl implements PermissionService {
         Set<UserRole> roles = userRepository.findOne(userId).getUserRoles();
         List<Permission> permissions = new ArrayList<>();
 
-        for (UserRole userRole: roles) {
-                Role role = userRole.getRole();
-                Set<RolePermission> rolePermList = role.getRolePermissions();
-            for (RolePermission rolePerm: rolePermList) {
+        for (UserRole userRole : roles) {
+            Role role = userRole.getRole();
+            Set<RolePermission> rolePermList = role.getRolePermissions();
+            for (RolePermission rolePerm : rolePermList) {
                 permissions.add(rolePerm.getPermission());
             }
 
         }
-        return permissions;
+        return permissions.stream()
+                .collect(collectingAndThen(toCollection(() -> new TreeSet<>(comparingLong(Permission::getId))),
+                        ArrayList::new));
     }
 }
